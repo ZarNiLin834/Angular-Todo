692b6424fd2f5d52f4f4ef59b3e554a6ed0552a8
ZarNiLin834 <smcjuejue2991999@gmail.com>
2025-02-07 09:26:22 +0630

    todo

--- todo-form/src/app/app.module.ts
+++ todo-form/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    TodoFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
